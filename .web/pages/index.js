ae32bebbff67230a0364951821de323e541272dd
Juan Jose Segui Borja <jjsegui@ideasmedioambientales.com>
2025-04-30 16:25:45 +0200

    Funcional con extras2

---
+++ .web/pages/index.js
@@ -0,0 +1,30 @@
+/** @jsxImportSource @emotion/react */
+
+
+import { Fragment } from "react"
+import { Box_ed6b6e6f81ebddadf45fab2b96c99f0f } from "$/utils/stateful_components"
+import NextHead from "next/head"
+
+
+
+export default function Component() {
+    
+
+
+
+
+  return (
+    <Fragment>
+
+<Box_ed6b6e6f81ebddadf45fab2b96c99f0f/>
+<NextHead>
+
+<title>
+
+{"Qr | Index"}
+</title>
+<meta content={"favicon.ico"} property={"og:image"}/>
+</NextHead>
+</Fragment>
+  )
+}
--- qr.py
+++
@@ -1,5 +0,0 @@
-import qrcode
-img = qrcode.make("https//www.qr.ideasmedioambientales.com/jjsegui")
-f = open("output.png", "wb")
-img.save(f)
-f.close()
