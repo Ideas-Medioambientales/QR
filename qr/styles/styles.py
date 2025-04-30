4b74d7564681daa8854a603147d22e7861a1e80e
Juan Jose Segui Borja <jjsegui@ideasmedioambientales.com>
2025-04-30 16:20:05 +0200

    Funcional con extras

--- qr/styles/styles.py
+++ qr/styles/styles.py
@@ -1,18 +1,6 @@
-import refles as rx
-from enum import Enum
+import reflex as rx
 
-class Color(Enum):
-    """Enum for color names."""
-    PRIMARY = "#86B73D"
-    SECONDARY = "#3E665C"
-    EXTRA = "#3C403E"
-    BACKGROUND = "#F1F1F1"
-    TEXT = "#3C403E"
-    
 
-BASE_STYLE = {
-    "background_color": Color.BACKGROUND.value, 
-    "text_color": Color.TEXT.value,
-    "primary_color": Color.PRIMARY.value
-    
-}
+STYLESHEET = [
+    "https://fonts.googleapis.com/css?family=Poppins&display=swap",
+]
